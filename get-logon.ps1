6f3fdaee1cbd52ef9c707ceec38feb666349bd8c
Stailz <stailzsanchez@gmail.com>
2023-04-05 09:19:06 +0300

    +

---
+++ get-logon.ps1
@@ -0,0 +1,42 @@
+
+
+# $user = "���������"
+
+
+function get-logon {
+  param ($user)
+  function GetLogonsUser($userName) {
+    $result = ((get-aduser -Filter "(Name -like '*$userName*')" -Properties info).info).split("#")
+    for ($i = 0; $i -lt $result.Count; $i += 1) {
+      "$i) $($result[$i])"
+    }
+  }
+
+  $listUsers = (Get-AdUser -Filter "(Name -like '*$user*')" -properties Name).Name
+
+
+  $countSearch = $listUsers.Count
+
+  if ($countSearch -eq 0) {
+    "Not found users - $user"
+  }
+  elseif ($countSearch -eq 1) {
+    GetLogonsUser -userName ($listUsers)
+  }
+  else {
+    for ($i = 1; $i -le $listUsers.Count; $i += 1) {
+      "$i) " + $listUsers[$i - 1]
+    }
+
+    $userNum = Read-Host -Prompt 'Input user number'
+
+    GetLogonsUser -userName $listUsers[$userNum - 1]
+  }
+
+
+
+
+
+}
+
+
